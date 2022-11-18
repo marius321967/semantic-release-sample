40ebca917c8812773eb0a41dd4f6af7fb2561ed7
marius321967 <marius.dras@gmail.com>
2022-11-18 20:27:17 +0000

    fix: add something, remove something

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 require('./feat1.js');
 
-console.log('Hello, world!');
-console.log('Hello, world!');
-console.log('Hello, world!');
-
 console.log('Are you ready for the new feature?');
 console.log('Now you are for sure');
 console.log('foo');
 console.log('bar');
+console.log('baz');
